fad9ed0a90ef3f9ecad3eb301515935e0bf62db0
Brianna Dukes <skylar.deep@yahoo.com>
2024-03-02 11:15:52 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+//Configure Tailwind to remove unused styles in production
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
